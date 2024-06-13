08132d79d2de6c617c94dfa3ea3c657cc69c481e
Chidubem Onwuchuluba <101866658+dubemmmm@users.noreply.github.com>
2024-06-13 14:26:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ Welcome to my professional portfolio. Here you can find information about my wor
 # Add a sidebar with navigation
 st.sidebar.title("Navigation")
 option = st.sidebar.selectbox("Go to", ["Home", "Work Experience", "Education", "Skills", "Projects"])
+# Home Section
+if option == "Home":
+    st.header("Welcome")
+    st.write("I am a motivated and dynamic individual driven by a result-oriented approach and a keen eagerness to learn.")
+    st.image("images/profile.jpg", width=300)  # Add your image here
+
+    st.write("## Connect with me")
+    st.markdown("[LinkedIn](https://www.linkedin.com/in/chidubem-onwuchuluba-787414227/)")
+    st.markdown("[GitHub](https://github.com/dubemmmm)")
+    st.markdown("[Email](mailto:onwuchulubachidubem@gmail.com)")
